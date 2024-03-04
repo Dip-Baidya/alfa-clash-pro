e6406aa7d727dd989c1c9a213e7db003362db46b
Dip-Baidya <dipbaidya.cse@gmail.com>
2024-03-04 19:30:47 +0600

    27-6 Change Home Screen To Playground By Click

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,13 @@
-function play() {
+// function play() {
+//     // Home Section Hidden
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
 
+//     // Show the Playground Section
+//     const playGroundSection = document.getElementById('play-ground');
+//     playGroundSection.classList.remove('hidden');
+// }
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
 }
